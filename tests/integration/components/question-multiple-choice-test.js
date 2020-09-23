6c7783c37df52a6c186e7b8b836630242cf03fff
GABRIEL FIEL <gabriel@paywithbolt.com>
2020-09-23 11:33:54 +0200

    Added basic test to specific data

--- tests/integration/components/question-multiple-choice-test.js
+++ tests/integration/components/question-multiple-choice-test.js
@@ -2,25 +2,39 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import Quiz from '../../../constants/quiz';
 
 module('Integration | Component | question-multiple-choice', function(hooks) {
   setupRenderingTest(hooks);
 
+
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    assert.expect(3);
 
-    await render(hbs`<QuestionMultipleChoice />`);
+    this.store = this.owner.lookup('service:store')
 
-    assert.equal(this.element.textContent.trim(), '');
+    let question = Quiz.questionnaire.questions[0]
+    let choices = question.choices
+    delete question.choices
 
-    // Template block usage:
-    await render(hbs`
-      <QuestionMultipleChoice>
-        template block text
-      </QuestionMultipleChoice>
-    `);
+    let questionTmp = this.store.createRecord('question', {id: question.identifier, image: Math.floor(Math.random() * 10) + 1, ...question} )
+    choices && choices.forEach(choice => {
+      this.store.createRecord('choice', {
+        question: questionTmp, 
+        label: choice.label, 
+        value: choice.value, 
+        selected: choice.selected
+      })
+    });
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    this.set('question', questionTmp);
+
+    await render(hbs`<QuestionMultipleChoice @question={{this.question}} />`);
+
+    assert.equal(this.element.querySelector('span.title').textContent.trim(), 'Wen möchtest Du versichern?');
+    assert.equal(this.element.querySelector('button.btn-primary').textContent.trim(), 'Next');
+    assert.equal(this.element.querySelectorAll('.btn-option').length, 5, '5 results rendered');
+    // row d-flex align-items-center justify-content-center h-100 c-snap
+
   });
 });
